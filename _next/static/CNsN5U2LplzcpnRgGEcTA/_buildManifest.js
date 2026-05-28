d932a9711fee2ebcc9eb09d70a7aa9f38f9357bf
nicolashjrmy <nicolashjeremy17@gmail.com>
2026-05-28 10:12:56 +0700

    Updates

---
+++ _next/static/CNsN5U2LplzcpnRgGEcTA/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/monitoring-dashboard//_next/:path+",
+        "destination": "/monitoring-dashboard/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
